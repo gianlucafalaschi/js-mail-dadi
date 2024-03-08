913c5447091e2acd07fc60cae16bbc5ffa674f15
gianlucafalaschi <gian100gian@outlook.it>
2024-03-08 15:34:44 +0100

    feat:  dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -47,3 +47,23 @@ if(randomNumberUser > randomNumberComputer) {
 
 
 // EMAIL
+
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Per la soluzione di questo esercizio NON è consentito utilizzare .includes di js!
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"  */
+
+// Chiedi all’utente la sua email,
+const userMail = prompt('Inserisci email');
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
